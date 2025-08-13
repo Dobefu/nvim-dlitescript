2167cd10b719d7db0b048ac50bef816bb6be765a
Connor van Spronssen <info@dobefu.com>
2025-08-13 17:53:58 +0200

    feat: Add a basic config with automatic TreeSitter highlights

--- lua/nvim-dlitescript/init.lua
+++ lua/nvim-dlitescript/init.lua
@@ -1,7 +1,66 @@
 ---@class DLiteScript
 local M = {}
 
-M.setup = function()
+local default_config = {
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+  fold = {
+    enable = false,
+  },
+  auto_install = true,
+}
+
+---@param opts table|nil
+M.setup = function(opts)
+  local config = vim.tbl_deep_extend("force", default_config, opts or {})
+
+  local has_treesitter, ts_configs = pcall(require, "nvim-treesitter.configs")
+
+  if not has_treesitter then
+    vim.notify(
+      "nvim-treesitter not found. Please install it to enable syntax highlighting.",
+      vim.log.levels.WARN
+    )
+
+    return
+  end
+
+  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  parser_config.dlitescript = {
+    install_info = {
+      url = "https://github.com/Dobefu/tree-sitter-dlitescript",
+      files = { "src/parser.c" },
+      branch = "main",
+    },
+    filetype = "dlitescript",
+  }
+
+  ts_configs.setup {
+    ensure_installed = config.auto_install and { "dlitescript" } or nil,
+    highlight = {
+      enable = config.highlight.enable,
+    },
+    indent = {
+      enable = config.indent.enable,
+    },
+    fold = {
+      enable = config.fold.enable,
+    },
+  }
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "dlitescript",
+    callback = function()
+      vim.opt_local.commentstring = "// %s"
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+      vim.opt_local.expandtab = true
+    end,
+  })
 end
 
 return M
